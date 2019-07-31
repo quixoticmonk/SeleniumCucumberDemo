5984703b518fd6693829a30b51df517717526b84
quixoticmonk <manuchandrasekhar@gmail.com>
2019-07-31 14:45:34 -0400

    Create drivers.sh

---
+++ drivers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir drivers
+cd drivers
+wget "https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip"
+unzip *.zip -d .
+ cd /home/travis/build/quixoticmonk/SeleniumCucumberDemo
